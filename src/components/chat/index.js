075a3d6ae8bb50a0e250ce612044311a2b0e38cc
Anthony Stenmark <anthony.stenmark@sca.com.au>
2022-09-15 22:04:39 +1000

    Initial chat

--- src/components/chat/index.js
+++ src/components/chat/index.js
@@ -1,9 +1,50 @@
+import { io } from "socket.io-client";
+import { useEffect, useState } from "react";
+import "./chat.modules.css";
+
+const socket = io("https://rocky-plateau-75193.herokuapp.com/");
+
 function Chat() {
-    return (
-      <>
-      </>
-    );
-  }
-  
-  export default Chat;
-  
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  // useEffect(() => {
+  //   console.log("conneting");
+  // }, []);
+
+  socket.on("chat message", function (msg) {
+    console.log(msg);
+    const messagesArr = [...messages];
+    messagesArr.push(msg);
+    setMessages(messagesArr);
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (message) {
+      socket.emit("chat message", message);
+      setMessage("");
+    }
+  };
+
+  return (
+    <div className="chatWrap">
+      <ul id="messages">
+        {messages.map((msg) => (
+          <li>{msg}</li>
+        ))}
+      </ul>
+      <form id="form" action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="message"
+          name="message"
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+        />
+        <button>Send</button>
+      </form>
+    </div>
+  );
+}
+
+export default Chat;
