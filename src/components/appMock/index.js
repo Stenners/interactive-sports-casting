8a866cfd77dd0c27537409525b8a867ae36babb6
Anthony Stenmark <anthony.stenmark@sca.com.au>
2022-09-15 13:28:29 +1000

    Styling updates

--- src/components/appMock/index.js
+++ src/components/appMock/index.js
@@ -1,9 +1,10 @@
-import styles from './appMock.modules.css'
+import './appMock.modules.css'
 import ChatButton from '../chatButton';
 
 function ListnrMock() {
   return (
     <div className="mock">
+      <div className='player'></div>
        <ChatButton className="chatBtn" />
     </div>
   );
