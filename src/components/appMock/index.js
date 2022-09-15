45dd0cd224a03d4f60731971e21c92ec9a769c1b
Anthony Stenmark <anthony.stenmark@sca.com.au>
2022-09-15 13:13:00 +1000

    Fixed css

--- src/components/appMock/index.js
+++ src/components/appMock/index.js
@@ -1,10 +1,10 @@
-import styles from './appMock.modules.css'
-import ChatButton from '../chatButton';
+import "./appMock.modules.css";
+import ChatButton from "../chatButton";
 
 function ListnrMock() {
   return (
     <div className="mock">
-       <ChatButton className="chatBtn" />
+      <ChatButton />
     </div>
   );
 }
